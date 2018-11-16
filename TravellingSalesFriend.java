cde875b9f71350774228313967120b340e94fbcb
MatthewJohnson257 <MJohnson5342@gmail.com>
2018-11-16 17:01:53 -0500

    Added some edge case handling, processing of input

--- TravellingSalesFriend.java
+++ TravellingSalesFriend.java
@@ -1,11 +1,75 @@
 import java.util.*;
 
-public class TravellingSalesFriend{
-    public static void main(String[] args){
+public class TravellingSalesFriend
+{
+    public static void main(String[] args)
+    {
         Scanner sc = new Scanner(System.in);
+        int numberOfCities = sc.nextInt();
+        System.out.println("numberOfCities: " + numberOfCities);
+
+        // Handle edge cases involving a very small number of cities
+        if(numberOfCities < 3)
+        {
+            handleEdgeCases(numberOfCities);
+        }
+
+        else
+        {
+            int[][] givenFlightCosts = new int[numberOfCities][numberOfCities];
+            for(int i = 0; i < numberOfCities; i++)
+            {
+                for(int j = 0; j < numberOfCities; j++)
+                {
+                    givenFlightCosts[i][j] = sc.nextInt();
+                }
+            }
+
+            printArray(givenFlightCosts);
 
 
-        
+            int[][] solutions = new int[numberOfCities][numberOfCities];
+            
+
+        }
+    }
+
+
+    public static void handleEdgeCases(int numberOfCities)
+    {
+        if(numberOfCities <= 0)
+        {
+            System.out.println(0);
+            System.out.println("There are no cities for me to visit.");
+            System.out.println("There are no cities for my friend to visit.");
+        }
+        else if(numberOfCities == 1)
+        {
+            System.out.println(0);
+            System.out.println(0);
+            System.out.println("There are no cities for my friend to visit.");
+        }
+        else if(numberOfCities == 2)
+        {
+            System.out.println(0);
+            System.out.println(0);
+            System.out.println(1);
+        }
+    }
+
+
+
+
+    public static void printArray(int[][] array)
+    {
+        for(int i = 0; i < array.length; i++)
+        {
+            for(int j = 0; j < array[0].length; j++)
+            {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 
 }
