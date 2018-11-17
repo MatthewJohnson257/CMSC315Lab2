91d049ec2b3317c25d3dc9e4c21754bab41a38a5
MatthewJohnson257 <MJohnson5342@gmail.com>
2018-11-17 14:32:09 -0500

    Began work on actual calculation of costs of different paths

--- TravellingSalesFriend.java
+++ TravellingSalesFriend.java
@@ -26,13 +26,66 @@ public class TravellingSalesFriend
             }
 
             printArray(givenFlightCosts);
+            System.out.println();
 
 
             int[][] solutions = new int[numberOfCities][numberOfCities];
-            
 
+            // first column
+            for(int k = 0; k < numberOfCities; k++)
+            {
+                solutions[k][0] = -1;
+            }
+
+            // first row
+            solutions[0][1] = 0;
+            for(int k = 2; k < numberOfCities; k++)
+            {
+                solutions[0][k] = solutions[0][k-1] + givenFlightCosts[k-1][k];
+            }
+
+            // second column
+            solutions[1][1] = -1;
+            solutions[2][1] = givenFlightCosts[0][2];
+            for(int k = 3; k < numberOfCities; k++)
+            {
+                solutions[k][1] = solutions[k-1][1] + givenFlightCosts[k-1][k];
+            }
+
+
+            for(int i = 1; i < numberOfCities; i++)
+            {
+                for(int j = 2; j < numberOfCities; j++)
+                {
+                    int tempMin = 99999;
+                    if(i == j)
+                    {
+                        solutions[i][j] = -1;
+                    }
+
+                    else if (i < j)
+                    {
+                        for(int k = 1; k < i; k++)
+                        {
+                            if(tempMin > solutions[i][k] + givenFlightCosts[k][i+1])
+                            {
+                                tempMin = solutions[i][k] + givenFlightCosts[k][i+1];
+                            }
+                            solutions[i][j] = tempMin + solutions[0][j] - solutions[0][i+1];
+                        }
+                    }
+
+
+                }
+            }
+
+
+            printArray(solutions);
         }
     }
+
+
+
 
 
     public static void handleEdgeCases(int numberOfCities)
@@ -66,7 +119,7 @@ public class TravellingSalesFriend
         {
             for(int j = 0; j < array[0].length; j++)
             {
-                System.out.print(array[i][j] + " ");
+                System.out.print(array[i][j] + "\t");
             }
             System.out.println();
         }
