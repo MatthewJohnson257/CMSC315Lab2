21f142df34c9a74649657023526e68e639f26524
MatthewJohnson257 <MJohnson5342@gmail.com>
2018-11-18 22:57:04 -0500

    Working better now, but does not correctly process lower number cities

--- TravellingSalesFriend.java
+++ TravellingSalesFriend.java
@@ -2,11 +2,14 @@ import java.util.*;
 
 public class TravellingSalesFriend
 {
+    static int index1 = 999;
+    static int index2 = 999;
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
         int numberOfCities = sc.nextInt();
         System.out.println("numberOfCities: " + numberOfCities);
+
 
         // Handle edge cases involving a very small number of cities
         if(numberOfCities < 3)
@@ -30,7 +33,9 @@ public class TravellingSalesFriend
 
 
             int[][] solutions = new int[numberOfCities][numberOfCities];
-            int[][] solutionTracking = new int[numberOfCities][numberOfCities];
+            //int[][] solutionTracking = new int[numberOfCities][numberOfCities];
+            int[][] trackingIndex1 = new int[numberOfCities][numberOfCities];
+            int[][] trackingIndex2 = new int[numberOfCities][numberOfCities];
 
             // first column
             for(int k = 0; k < numberOfCities; k++)
@@ -53,19 +58,26 @@ public class TravellingSalesFriend
                 solutions[k][1] = solutions[k-1][1] + givenFlightCosts[k-1][k];
             }
 
+            // for(int k = 1; k < numberOfCities; k++)
+            // {
+            //     solutions[k-1][k] =
+            // }
+            //boolean foundFriendStartPosition = false;
             int tempMin = 99999;
 
             for(int i = 1; i < numberOfCities; i++)
             {
                 for(int j = 2; j < numberOfCities; j++)
                 {
+                    //if(!(i == 0 && j == 1) || !(i == 2 && j == 1))
+                    //{
                     tempMin = 99999;
                     if(i == j)
                     {
                         solutions[i][j] = -1;
                     }
 
-                    else if (i < j)
+                    else if (i < j && solutions[i][j] < 1)
                     {
                         if(i == 1)
                         {
@@ -74,7 +86,9 @@ public class TravellingSalesFriend
                                 if((tempMin > solutions[i][k] + givenFlightCosts[k][i+1]) && i != k)
                                 {
                                     tempMin = solutions[i][k] + givenFlightCosts[k][i+1];
-                                    // solutionTracking[i][j] = (i * 10) + k;
+                                    //solutionTracking[i][j] = (i * 10) + k;
+                                    trackingIndex1[i][j] = i;
+                                    trackingIndex2[i][j] = k;
                                 }
                                 solutions[i][j] = tempMin + solutions[0][j] - solutions[0][i+1];
 
@@ -89,28 +103,48 @@ public class TravellingSalesFriend
                                 if(tempMin > solutions[i][k] + givenFlightCosts[k][i+1])
                                 {
                                     tempMin = solutions[i][k] + givenFlightCosts[k][i+1];
-                                    // solutionTracking[i][j] = (i * 10) + k;
+                                    //solutionTracking[i][j] = (i * 10) + k;
+                                    trackingIndex1[i][j] = i;
+                                    trackingIndex2[i][j] = k;
                                 }
                                 solutions[i][j] = tempMin + solutions[0][j] - solutions[0][i+1];
                             }
                         }
                     }
 
-                    else if(i > j)
+                    else if(i > j && solutions[i][j] < 1)
                     {
+                        if(j == 1)
+                        {
+                            for(int k = 0; k < j; k++)
+                            {
+                                if((tempMin > solutions[k][j] + givenFlightCosts[k][j+1]))
+                                {
+                                    tempMin = solutions[k][j] + givenFlightCosts[k][j+1];
+                                    //solutionTracking[i][j] = (i * 10) + k;
+                                    trackingIndex1[i][j] = k;
+                                    trackingIndex2[i][j] = j;
+                                }
+                                solutions[i][j] = tempMin + solutions[i][1] - solutions[j+1][1];
+
+
+                            }
+                        }
+
 
                         for(int k = 0; k < j; k++)
                         {
                             if(tempMin > solutions[k][j] + givenFlightCosts[k][j+1])
                             {
                                 tempMin = solutions[k][j] + givenFlightCosts[k][j+1];
-                                // solutionTracking[i][j] = (k * 10) + j;
-
+                                //solutionTracking[i][j] = (k * 10) + j;
+                                trackingIndex1[i][j] = k;
+                                trackingIndex2[i][j] = j;
                             }
                             solutions[i][j] = tempMin + solutions[i][1] - solutions[j+1][1];
                         }
                     }
-
+                    //}
                     //System.out.println("tempMin: " + tempMin);
                 }
             }
@@ -119,25 +153,149 @@ public class TravellingSalesFriend
 
 
 
+
             int finalMinimumCost = findCostOfSolution(solutions);
             System.out.println("finalMinimumCost: " + finalMinimumCost);
 
+            System.out.println();
+            //printArray(solutionTracking);
+            printArray(trackingIndex1);
+            System.out.println();
+            printArray(trackingIndex2);
 
 
             // needs FINISHED!!
 
-            // int i = 999;
-            // int j = 999;
-            // i = solutionTracking[20][23] / 10;
-            // j = solutionTracking[20][23] % 10;
-            // while(i != 0)
+
+            // int[] myCities = new int[numberOfCities];
+            // int[] friendCities = new int[numberOfCities];
+
+            ArrayList<Integer> myCitiesList = new ArrayList<Integer>();
+            ArrayList<Integer> friendCitiesList = new ArrayList<Integer>();
+
+
+            // index1 = solutionTracking[index1][index2] / 10;
+            // index2 = solutionTracking[index1][index2] % 10;
+
+            myCitiesList.add(index1);
+            friendCitiesList.add(index2);
+
+            if(index1 < index2)
+            {
+
+                for(int l = index1 + 1; l < index2; l++)
+                {
+                    System.out.println("First: " + l);
+                    friendCitiesList.add(l);
+                }
+
+            }
+
+
+            else if(index1 > index2)
+            {
+                for(int l = index2 + 1; l < index1; l++)
+                {
+                    System.out.println("Second: " + l);
+                    myCitiesList.add(l);
+                }
+            }
+
+
+
+            while(index1 != 0)
+            {
+                int tempIndex1 = trackingIndex1[index1][index2];
+                int tempIndex2 = trackingIndex2[index1][index2];
+                // myCitiesList.add(index1);
+                // friendCitiesList.add(index2);
+
+                if(tempIndex1 < tempIndex2)
+                {
+                    myCitiesList.add(tempIndex1);
+                    for(int l = tempIndex1 + 1; l < tempIndex2; l++)
+                    {
+                        System.out.println("First: " + l);
+                        friendCitiesList.add(l);
+                    }
+
+                }
+
+
+                else if(tempIndex1 > tempIndex2)
+                {
+                    friendCitiesList.add(tempIndex2);
+                    for(int l = tempIndex2 + 1; l < tempIndex1; l++)
+                    {
+                        System.out.println("Second: " + l);
+                        myCitiesList.add(l);
+                    }
+                }
+
+                index1 = tempIndex1;
+                index2 = tempIndex2;
+            }
+
+
+            // myCitiesList.add(index1);
+            // friendCitiesList.add(index2);
+
+
+            // while(index1 != 0)
             // {
-            //     sol
+            //     myCitiesList.add(index1);
+            //     friendCitiesList.add(index2);
+            //
+            //     if(index1 < index2)
+            //     {
+            //         //myCitiesList.add(index1);
+            //         for(int l = index1 + 1; l < index2; l++)
+            //         {
+            //             System.out.println("First: " + l);
+            //             friendCitiesList.add(l);
+            //         }
+            //
+            //     }
+            //
+            //
+            //     else if(index1 > index2)
+            //     {
+            //         //friendCitiesList.add(index2);
+            //         for(int l = index2 + 1; l < index1; l++)
+            //         {
+            //             System.out.println("Second: " + l);
+            //             myCitiesList.add(l);
+            //         }
+            //     }
+            //     System.out.println("index1BEFOR: " + index1);
+            //     System.out.println("index2BEFOR: " + index2);
+            //     index1 = trackingIndex1[index1][index2];
+            //     index2 = trackingIndex2[index1][index2];
+            //     System.out.println("index1AFTER: " + index1);
+            //     System.out.println("index2AFTER: " + index2);
+            //
+            //     System.out.println("SJDIOFJEWIORJOEWJOI" + trackingIndex2[index1][index2]);
             // }
 
 
+            // 222
+            // 0 4 7 9 12 17 20
+            // 56
+            // 1 2 3 5 6 8 10 11 13 14 15 16 18 19 21 22 23
+            // 166
 
 
+
+
+
+
+
+
+            Collections.sort(myCitiesList);
+            Collections.sort(friendCitiesList);
+
+            System.out.println("MyCities: " + myCitiesList.toString());
+            System.out.println("FriendsCities: " + friendCitiesList.toString());
 
 
 
@@ -154,14 +312,14 @@ public class TravellingSalesFriend
         if(numberOfCities <= 0)
         {
             System.out.println(0);
-            System.out.println("There are no cities for me to visit.");
-            System.out.println("There are no cities for my friend to visit.");
+            System.out.println();
+            System.out.println();
         }
         else if(numberOfCities == 1)
         {
             System.out.println(0);
             System.out.println(0);
-            System.out.println("There are no cities for my friend to visit.");
+            System.out.println();
         }
         else if(numberOfCities == 2)
         {
@@ -180,12 +338,18 @@ public class TravellingSalesFriend
             if(minTotalCost > array[k][array.length-1] && array[k][array.length-1] != -1)
             {
                 minTotalCost = array[k][array.length-1];
+                index1 = k;
+                index2 = array.length - 1;
             }
             if(minTotalCost > array[array.length-1][k] && array[array.length-1][k] != -1)
             {
                 minTotalCost = array[array.length-1][k];
+                index1 = array.length - 1;
+                index2 = k;
             }
         }
+        System.out.println("index1: " + index1);
+        System.out.println("index2: " + index2);
         return minTotalCost;
     }
 
