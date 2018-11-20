8100feaf708711b66b80917300bffae03f6fc003
MatthewJohnson257 <MJohnson5342@gmail.com>
2018-11-19 19:14:02 -0500

    Added changes, correctly computes the total cost, but not which cities each person takes

--- TravellingSalesFriend.java
+++ TravellingSalesFriend.java
@@ -36,115 +36,218 @@ public class TravellingSalesFriend
             int[][] trackingIndex1 = new int[numberOfCities][numberOfCities];
             int[][] trackingIndex2 = new int[numberOfCities][numberOfCities];
 
-            // first column
-            for(int k = 0; k < numberOfCities; k++)
-            {
-                solutions[k][0] = -1;
-            }
+            // // first column
+            // for(int k = 0; k < numberOfCities; k++)
+            // {
+            //     solutions[k][0] = -1;
+            // }
+            //
+            // // first row
+            // solutions[0][1] = 0;
+            // for(int k = 2; k < numberOfCities; k++)
+            // {
+            //     solutions[0][k] = solutions[0][k-1] + givenFlightCosts[k-1][k];
+            // }
+            //
+            //
+            //
+            // for(int k = 2; k < numberOfCities - 1; k++)
+            // {
+            //     solutions[k][k+1] = solutions[0][1] + solutions[0][k] - givenFlightCosts[k-1][k] + givenFlightCosts[0][1];
+            // }
+            //
+            // // second column
+            // solutions[1][1] = -1;
+            // solutions[2][1] = givenFlightCosts[0][2];
+            // solutions[1][2] = givenFlightCosts[0][1];
+            // for(int k = 3; k < numberOfCities; k++)
+            // {
+            //     solutions[k][1] = solutions[k-1][1] + givenFlightCosts[k-1][k];
+            // }
+            //
+            // // for(int k = 2; k < numberOfCities; k++)
+            // // {
+            // //     solutions[k-1][k] = givenFlightCosts[0][1] + solutions[0][k] - givenFlightCosts[k-1][k];
+            // // }
+            //
+            // // for(int k = 1; k < numberOfCities; k++)
+            // // {
+            // //     solutions[k-1][k] =
+            // // }
+            // int tempMin = maximumCostFromOneCityToAnother * maximumCostFromOneCityToAnother;
+            //
+            // for(int i = 1; i < numberOfCities; i++)
+            // {
+            //     for(int j = 1; j < numberOfCities; j++)
+            //     {
+            //         tempMin = maximumCostFromOneCityToAnother * maximumCostFromOneCityToAnother;
+            //         if(i == j)
+            //         {
+            //             solutions[i][j] = -1;
+            //         }
+            //
+            //         else if (i < j && solutions[i][j] < 1)
+            //         {
+            //             if(i == 1)
+            //             {
+            //                 for(int k = 0; k < i; k++)
+            //                 {
+            //                     if((tempMin > solutions[1][k] + givenFlightCosts[i+1][k]) && i != k  && !(i == numberOfCities))
+            //                     {
+            //                         tempMin = solutions[i][k] + givenFlightCosts[k][i+1];
+            //                         trackingIndex1[i][j] = i;
+            //                         trackingIndex2[i][j] = k;
+            //                     }
+            //                     solutions[i][j] = tempMin + solutions[0][j] - solutions[0][i+1];
+            //
+            //
+            //                 }
+            //             }
+            //
+            //             else
+            //             {
+            //                 for(int k = 1; k < i; k++)
+            //                 {
+            //                     if(tempMin > solutions[i][k] + givenFlightCosts[k][i+1])
+            //                     {
+            //                         tempMin = solutions[i][k] + givenFlightCosts[k][i+1];
+            //                         trackingIndex1[i][j] = i;
+            //                         trackingIndex2[i][j] = k;
+            //                     }
+            //                     solutions[i][j] = tempMin + solutions[0][j] - solutions[0][i+1];
+            //                 }
+            //             }
+            //         }
+            //
+            //         else if(i > j && solutions[i][j] < 1)
+            //         {
+            //             if(j == 1)
+            //             {
+            //                 for(int k = 0; k < j; k++)
+            //                 {
+            //                     if((tempMin > solutions[k][j] + givenFlightCosts[k][j+1]))
+            //                     {
+            //                         tempMin = solutions[k][j] + givenFlightCosts[k][j+1];
+            //                         trackingIndex1[i][j] = k;
+            //                         trackingIndex2[i][j] = j;
+            //                     }
+            //                     solutions[i][j] = tempMin + solutions[i][1] - solutions[j+1][1];
+            //
+            //
+            //                 }
+            //             }
+            //
+            //             else
+            //             {
+            //
+            //             for(int k = 0; k < j; k++)
+            //             {
+            //                 if(tempMin > solutions[k][j] + givenFlightCosts[k][j+1])
+            //                 {
+            //                     tempMin = solutions[k][j] + givenFlightCosts[k][j+1];
+            //                     trackingIndex1[i][j] = k;
+            //                     trackingIndex2[i][j] = j;
+            //                 }
+            //                 solutions[i][j] = tempMin + solutions[i][1] - solutions[j+1][1];
+            //             }
+            //             }
+            //         }
+            //         //System.out.println("tempMin: " + tempMin);
+            //     }
+            // }
+
+            int tempMin = Integer.MAX_VALUE;
+
+            solutions[0][0] = 0;
+            solutions[0][1] = 0;
 
             // first row
-            solutions[0][1] = 0;
             for(int k = 2; k < numberOfCities; k++)
             {
                 solutions[0][k] = solutions[0][k-1] + givenFlightCosts[k-1][k];
             }
 
-            // second column
-            solutions[1][1] = -1;
-            solutions[2][1] = givenFlightCosts[0][2];
-            solutions[1][2] = givenFlightCosts[0][1];
-            for(int k = 3; k < numberOfCities; k++)
+            // first column
+            for(int k = 1; k < numberOfCities; k++)
             {
-                solutions[k][1] = solutions[k-1][1] + givenFlightCosts[k-1][k];
+                solutions[k][0] = solutions[k-1][0] + givenFlightCosts[k-1][k];
             }
-
-            // for(int k = 2; k < numberOfCities; k++)
-            // {
-            //     solutions[k-1][k] = givenFlightCosts[0][1] + solutions[0][k] - givenFlightCosts[k-1][k];
-            // }
-
-            // for(int k = 1; k < numberOfCities; k++)
-            // {
-            //     solutions[k-1][k] =
-            // }
-            int tempMin = 99999;
 
             for(int i = 1; i < numberOfCities; i++)
             {
-                for(int j = 2; j < numberOfCities; j++)
+                for(int j = 1; j < numberOfCities; j++)
                 {
-                    tempMin = 99999;
+                    tempMin = Integer.MAX_VALUE;
+
                     if(i == j)
                     {
                         solutions[i][j] = -1;
                     }
 
-                    else if (i < j && solutions[i][j] < 1)
+                    else if(i < j)
                     {
                         if(i == 1)
                         {
-                            for(int k = 0; k < i; k++)
-                            {
-                                if((tempMin > solutions[i][k] + givenFlightCosts[k][i+1]) && i != k)
-                                {
-                                    tempMin = solutions[i][k] + givenFlightCosts[k][i+1];
-                                    trackingIndex1[i][j] = i;
-                                    trackingIndex2[i][j] = k;
-                                }
-                                solutions[i][j] = tempMin + solutions[0][j] - solutions[0][i+1];
-
-
-                            }
+                            solutions[i][j] = givenFlightCosts[0][1] + solutions[j][0] - solutions[2][0];
+                            trackingIndex1[i][j] = 0;
+                            trackingIndex2[i][j] = 2;
                         }
 
                         else
                         {
+
                             for(int k = 1; k < i; k++)
                             {
-                                if(tempMin > solutions[i][k] + givenFlightCosts[k][i+1])
+                                if(tempMin > solutions[i][k] + givenFlightCosts[k][i+1] + (solutions[0][j] - solutions[0][i+1]))
                                 {
-                                    tempMin = solutions[i][k] + givenFlightCosts[k][i+1];
+                                    tempMin = solutions[i][k] + givenFlightCosts[k][i+1] + (solutions[0][j] - solutions[0][i+1]);
                                     trackingIndex1[i][j] = i;
                                     trackingIndex2[i][j] = k;
                                 }
-                                solutions[i][j] = tempMin + solutions[0][j] - solutions[0][i+1];
+                                solutions[i][j] = tempMin;
                             }
-                        }
-                    }
 
-                    else if(i > j && solutions[i][j] < 1)
-                    {
-                        if(j == 1)
-                        {
-                            for(int k = 0; k < j; k++)
+                            if((j == i + 1) && (tempMin > solutions[i][0]))
                             {
-                                if((tempMin > solutions[k][j] + givenFlightCosts[k][j+1]))
-                                {
-                                    tempMin = solutions[k][j] + givenFlightCosts[k][j+1];
-                                    trackingIndex1[i][j] = k;
-                                    trackingIndex2[i][j] = j;
-                                }
-                                solutions[i][j] = tempMin + solutions[i][1] - solutions[j+1][1];
-
-
-                            }
-                        }
-
-
-                        for(int k = 0; k < j; k++)
-                        {
-                            if(tempMin > solutions[k][j] + givenFlightCosts[k][j+1])
-                            {
-                                tempMin = solutions[k][j] + givenFlightCosts[k][j+1];
-                                trackingIndex1[i][j] = k;
+                                tempMin = solutions[i][0];
+                                solutions[i][j] = tempMin;
+                                trackingIndex1[i][j] = i;
                                 trackingIndex2[i][j] = j;
                             }
-                            solutions[i][j] = tempMin + solutions[i][1] - solutions[j+1][1];
+                            //solutions[i][j] = tempMin + (solutions[0][j] - solutions[0][i+1]);
                         }
                     }
-                    //System.out.println("tempMin: " + tempMin);
+
+                    else if(i > j)
+                    {
+                        for(int k = 0; k < j; k++)
+                        {
+                            if(tempMin > solutions[k][j] + givenFlightCosts[k][j+1] + (solutions[i][0] - solutions[j+1][0]))
+                            {
+                                tempMin = solutions[k][j] + givenFlightCosts[k][j+1] + (solutions[i][0] - solutions[j+1][0]);
+                                trackingIndex1[i][j] = i;
+                                trackingIndex2[i][j] = k;
+                            }
+                            solutions[i][j] = tempMin;
+                        }
+
+                        if(tempMin > solutions[i][0] - givenFlightCosts[j-1][j] - givenFlightCosts[j][j+1] + givenFlightCosts[j-1][j+1])
+                        {
+                            tempMin = solutions[i][0] - givenFlightCosts[j-1][j] - givenFlightCosts[j][j+1] + givenFlightCosts[j-1][j+1];
+                            solutions[i][j] = tempMin;
+                            trackingIndex1[i][j] = i;
+                            trackingIndex2[i][j] = j;
+                        }
+                        //solutions[i][j] = tempMin + (solutions[i][0] - solutions[j+1][0]);
+                    }
+
                 }
             }
+
+
+
+
+
 
             printArray(solutions);
 
